ab314b919aff1e9e0cc70a9bf3e68f1d26853711
luis-pelozo <pelozoluis52@gmail.com>
2020-06-10 13:47:16 -0300

    commit 10-06-20

--- server.js
+++ server.js
@@ -2,12 +2,21 @@
 const express = require("express")
 const server = express()
 
+//Configurar arquivo estáticos 
+server.use(express.static("public"))
+
+
 //Criei uma rota/
 //capturo o pedido do cliente 
 server.get("/", function(req, res) {
     return res.sendFile(__dirname + "/index.html")
-    //console.log("aqui")
+})
+
+server.get("/ideias", function (req, res) {
+    return res.sendFile(__dirname + "/ideias.html")
 })
 
 //liguei o servidor na porta 3000
-server.listen(300)
+server.listen(300) 
+
+//4 - 53:09
